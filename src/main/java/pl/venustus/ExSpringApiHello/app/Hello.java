05cc3ed8eb88244866d63251cbb92ea69ef26bfd
venustuspl <venustus.pl@gmail.com>
2020-03-02 20:58:47 +0100

    0.0.6
    Signed-off-by: venustuspl <venustus.pl@gmail.com>

--- src/main/java/pl/venustus/ExSpringApiHello/app/Hello.java
+++ src/main/java/pl/venustus/ExSpringApiHello/app/Hello.java
@@ -3,6 +3,7 @@ package pl.venustus.ExSpringApiHello.app;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,16 +12,28 @@ import java.util.stream.Stream;
 @RestController
 public class Hello {
 
-    @GetMapping("/do")
-    public void sendDataToElectronicApps() {
+    @GetMapping("/getElectronicsData")
+    public void getElectronicsData() {
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<Computer[]> responseEntity = restTemplate.exchange("https://localhost:8080/api/computers/get",
+        ResponseEntity<Computer[]> responseEntity = restTemplate.exchange("http://localhost:8080/api/computers/get",
                 HttpMethod.GET,
                 null,
                 Computer[].class);
 
         Stream.of(responseEntity.getBody()).forEach(System.out::println);
 
+    }
+
+    @GetMapping("/deleteElectronicData")
+    public void deleteElectronicData(@RequestParam long removeId) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<Boolean> responseEntity = restTemplate.exchange("http://localhost:8080/api/computers/delete?id=" + removeId,
+                HttpMethod.DELETE,
+                null,
+                Boolean.class);
+
+        System.out.println(responseEntity.getBody());
     }
 
     @GetMapping("/")
