7fcadb604d4abb4ef0538fbc7bf8c96e95c032ec
venustuspl <venustus.pl@gmail.com>
2020-03-02 21:09:37 +0100

    0.0.7
    Signed-off-by: venustuspl <venustus.pl@gmail.com>

--- src/main/java/pl/venustus/ExSpringApiHello/app/Hello.java
+++ src/main/java/pl/venustus/ExSpringApiHello/app/Hello.java
@@ -2,6 +2,7 @@ package pl.venustus.ExSpringApiHello.app;
 
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,11 +25,15 @@ public class Hello {
 
     }
 
-    @GetMapping("/deleteElectronicData")
+    @DeleteMapping("/deleteElectronicData")
     public void deleteElectronicData(@RequestParam long removeId) {
         RestTemplate restTemplate = new RestTemplate();
 
-        ResponseEntity<Boolean> responseEntity = restTemplate.exchange("http://localhost:8080/api/computers/delete?id=" + removeId,
+        String url = "http://localhost:8080/api/computers/delete?id=" + removeId;
+
+        System.out.println(url);
+
+        ResponseEntity<Boolean> responseEntity = restTemplate.exchange(url,
                 HttpMethod.DELETE,
                 null,
                 Boolean.class);
